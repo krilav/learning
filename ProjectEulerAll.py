ed353ba6f6e6047a96e052a933e66b25238987f2
krilav <32963337+krilav@users.noreply.github.com>
2019-06-18 12:10:22 +0700

    end of 4 task

--- ProjectEulerAll.py
+++ ProjectEulerAll.py
@@ -1,7 +1,4 @@
-
 import time
-
-start = time.time()
 
 
 def project_euler_1(n, index=1):
@@ -41,18 +38,19 @@ def project_euler_2(n, index=1):
             sum_fib += a[i - 1]
 
     if index == 1:
-        print('Последнее число ряда Фибоначи, не привышающее {0} = {1}, а сумма ряда = {2}'.format(n, a[i-1], sum_fib))
+        print(
+            'Последнее число ряда Фибоначи, не привышающее {0} = {1}, а сумма ряда = {2}'.format(n, a[i - 1], sum_fib))
         print('Время работы функции - ', time.time() - start_time)
 
     return sum_fib
 
 
 def project_euler_3(n, index=1):
-    """Проект Эйлера, Задача 2.
+    """Проект Эйлера, Задача 3.
 
        Простые делители числа 13195 - это 5, 7, 13 и 29.
        Каков самый большой делитель числа 600851475143, являющийся простым числом?"""
-    
+
     start_time = time.time()
     old_n = n
     i = 2
@@ -68,7 +66,58 @@ def project_euler_3(n, index=1):
     return i
 
 
+def project_euler_4(n, index=1):
+    """Проект Эйлера, Задача 4.
+
+       Число-палиндром с обеих сторон (справа налево и слева направо) читается одинаково.
+       Самое большое число-палиндром, полученное умножением двух двузначных чисел – 9009 = 91 × 99.
+       Найдите самый большой палиндром, полученный умножением двух трехзначных чисел."""
+
+    start_time = time.time()
+    all_pol = []
+
+    for h1 in range(10*(n-1), 10**n):
+
+        for h2 in range(10*(n-1), 10**n):
+            k = 0
+            pr_chis = h1 * h2
+            test_chis = str(pr_chis)
+            light_chis = len(test_chis)
+
+            for i in range(light_chis // 2):
+                if test_chis[i] == test_chis[light_chis - i - 1]:
+                    k = k + 1
+                else:
+                    break
+
+            if k == light_chis // 2:
+                all_pol.append(pr_chis)
+
+    if index == 1:
+        print('Время работы функции - ', time.time() - start_time)
+        print('Максимальный число-палиндром из ', n, ' значных чмсел = ', max(all_pol))
+
+    return max(all_pol)
+
+
+def project_euler_5(n, index=1):
+    """Проект Эйлера, Задача 5.
+
+       2520 - самое маленькое число, которое делится без остатка на все числа от 1 до 10.
+       Какое самое маленькое число делится нацело на все числа от 1 до 20?"""
+
+    start_time = time.time()
+
+    if index == 1:
+        print('Время работы функции - ', time.time() - start_time)
+        print(n)
+
+    return
+
+
 # project_euler_1(1000, 0)
 # project_euler_2(4000000, 0)
-project_euler_3(600851475143, 1)
+# project_euler_3(600851475143, 1)
+# project_euler_4(3, 1)
+# project_euler_5()
 # print(project_euler_2.__doc__)
--- test.py
+++ test.py
@@ -1,2 +1,21 @@
+import time
+
+start_time = time.time()
+
+def is_polindrom(num):
+    raw = str(num)
+    r_num = int(raw[::-1])
+    if num == r_num:
+        return True
+    else:
+        return False
 
 
+pol = []
+
+for i in range(100,1000):
+    for j in range(100,1000):
+        if is_polindrom(i * j):
+            pol.append(i*j)
+print(max(pol))
+print('Время работы функции - ', time.time() - start_time)
